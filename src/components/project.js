0a7b806e58a9e1a502f6655b7f74dc54dcaef8c2
alexuschristensen <alexusdc6@gmail.com>
2022-12-05 22:54:46 -0800

    added photos

---
+++ src/components/project.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { capitalizeFirstLetter } from '../utils/helpers';
+import photo from "../assets/images/horiseon.png";
+import photo2 from '../assets/images/plant-scape.png';
+import photo3 from '../assets/images/calendar-method.png';
+import photo4 from '../assets/images/smart-homie.png';
+
+
+function Project(props) {
+  const currentCategory = {
+    name: "project 1",
+    description:
+      "Project 1",
+  };
+  return (
+    <section>
+      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.name}</p>
+      <div>
+        <img 
+            src={photo}
+            alt="Horiseon"
+            className="img-thumbnail mx-1" />
+
+        <img
+            src={photo2}
+            alt='Plant-scape'
+            className="img-thumbnail mx-1" />
+
+        <img
+            src={photo3}
+            alt="calendar-method"
+            className="img-thumbnail mx-1" />
+
+        <img
+            src={photo4}
+            alt="smart homie"
+            classname="img-thumbnail mx-1" />
+      </div>
+    </section>
+  );
+}
+export default Project;
